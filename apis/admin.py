4539532930a834abd215aa5b43389c1ae469539d
Brian de Heus <me@brian.jp>
2025-12-03 18:32:52 +0900

    Adds admin for API keys

--- apis/admin.py
+++ apis/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.conf import settings
+from .models import APIKey
+from .methods import generate_secure_string
 
-# Register your models here.
+
+@admin.register(APIKey)
+class APIKeyAdmin(admin.ModelAdmin):
+    list_display = ["id", "description", "user", "key"]
+    search_fields = ["description", "key", "user__username", "user__email"]
+    list_filter = ["user"]
+    autocomplete_fields = ["user"]
+    readonly_fields = ["key"]
+
+    def get_fields(self, request, obj=None):
+        if obj:
+            return ["description", "user", "key"]
+        else:
+            return ["description", "user"]
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.key = f"{settings.API_KEY_PREFIX}-{generate_secure_string(n=settings.API_KEY_LENGTH)}"
+        super().save_model(request, obj, form, change)
