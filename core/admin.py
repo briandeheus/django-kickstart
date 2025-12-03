5b297cd5881409be07695d42be961edc597b7102
Brian de Heus <me@brian.jp>
2025-12-03 18:28:23 +0900

    Adds admin page for CoreUser

---
+++ core/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CoreUser
+
+
+@admin.register(CoreUser)
+class CoreUserAdmin(UserAdmin):
+    list_display = [
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    ]
+    search_fields = ["username", "email", "first_name", "last_name"]
+    list_filter = ["is_staff", "is_superuser", "is_active", "groups"]
